9b7a75eb96a2da3683004966ad047c3e58609bce
Tareq <tareqaldhaifani@gmail.com>
2024-02-21 08:13:10 +0400

    correcetd routes link

--- website/views.py
+++ website/views.py
@@ -29,35 +29,35 @@ def send_message(msg):
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
-@views.route("/home")
-@views.route("/home/")
+@views.route("/flaskapp/")
+@views.route("/flaskapp/home")
+@views.route("/flaskapp/home/")
 def redirect_lang():
     return render_template("landing_page.html")
 
 
-@views.route("/en")
-@views.route("/en/")
-@views.route("/en/home")
-@views.route("/en/home/")
+@views.route("/flaskapp/en")
+@views.route("/flaskapp/en/")
+@views.route("/flaskapp/en/home")
+@views.route("/flaskapp/en/home/")
 def home_en():
     return render_template("home.html")
 
 
-@views.route("/en/who_we_are")
-@views.route("/en/who_we_are/")
+@views.route("/flaskapp/en/who_we_are")
+@views.route("/flaskapp/en/who_we_are/")
 def who_we_are_en():
     return render_template("who_we_are.html")
 
 
-@views.route("/en/how_to_use")
-@views.route("/en/how_to_use/")
+@views.route("/flaskapp/en/how_to_use")
+@views.route("/flaskapp/en/how_to_use/")
 def how_to_use_en():
     return render_template("how_to_use.html")
 
 
-@views.route("/en/order_now", methods=["GET", "POST"])
-@views.route("/en/order_now/", methods=["GET", "POST"])
+@views.route("/flaskapp/en/order_now", methods=["GET", "POST"])
+@views.route("/flaskapp/en/order_now/", methods=["GET", "POST"])
 def order_now_en():
     if request.method == "POST":
         data = request.form
@@ -69,13 +69,13 @@ def order_now_en():
     return render_template("order_now.html")
 
 
-@views.route("/en/contact_us")
-@views.route("/en/contact_us/")
+@views.route("/flaskapp/en/contact_us")
+@views.route("/flaskapp/en/contact_us/")
 def contact_us_en():
     return render_template("contact_us.html")
 
 
-@views.route("/en/terms_and_conditions")
-@views.route("/en/terms_and_conditions/")
+@views.route("/flaskapp/en/terms_and_conditions")
+@views.route("/flaskapp/en/terms_and_conditions/")
 def terms_and_conditions_en():
     return render_template("terms_and_conditions.html")
